7cde07d4274cf70b37b19795760cc75f3e05fe7a
Rahul Bhola <rahulbhola@gmx.com>
2017-09-08 20:30:50 +0530

    Code to create a connection instance to irc server

---
+++ routes/session.js
@@ -0,0 +1,26 @@
+// Require all external lib
+const express = require('express');
+const session = require('../lib/session');
+
+// Get instance to router
+var router = express.Router();
+
+/**
+ * Create new session connection
+ */
+router.post("/new",(req,res,next)=>{
+    var sess_id = new session("irc.evilcorp.ga",6667);
+    sess_id
+        .create_new()
+        .then(() => sess_id.commit_connection() )
+        .then(() => sess_id.attach_handle() )
+        .then(() => {
+            res.send("Connected ...");
+        })
+        .catch((err)=> {
+            res.status(400).send(err.message);
+        });
+});
+
+
+module.exports = router;
